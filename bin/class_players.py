404efa294ee4f2d908df33f5fc661b0531a7ed27
Igor Koltakov <igor-koltakov@yandex.ru>
2024-07-13 12:40:44 +0300

    test1

---
+++ bin/class_players.py
@@ -0,0 +1,25 @@
+class Players(SuperClass):
+    def __init__(self, name, health, armor, attack):
+        self.name = name
+        self.health = health
+        self.armor = armor
+        self.attack = attack
+
+class Warrior:
+    def __init__(self, name, ultra_attack):
+        self.name = warrior
+        self.ultra_attack = ultra_attack
+        super().__init__(name, health, armor, attack)
+
+class Rouge:
+    def __init__(self, name, ultra_skip):
+        self.name = Rouge
+        self.ultra_attack = ultra_skip
+        super().__init__(name, health, armor, attack)
+
+class Wizard
+    def __init__(self, name, ultra_health):
+        self.name = Wizard
+        self.ultra_health = ultra_health
+        super().__init__(name, health, armor, attack)
+
---
+++ bin/create_players.py
@@ -0,0 +1,21 @@
+#import class from class_players
+
+print('Введите свое имя, Игорок 1')
+player1_name = input()
+print('Приветствуем тебя,' + player1_name)
+print('Введите свое имя, Игорок 2')
+player2_name = input()
+print('Приветствуем тебя,' + player2_name)
+
+print('Выберите класс своего персонажа,' + player1_name + '!' + '\nВыбери из предложенных ниже классов')
+print('Warrior'+'\nRouge'+'\nWizard')
+player1_class = input()
+if player1_class == 'Warrior':
+    player1_class = Warrior
+elif player1_class == 'Rogue':
+    player1_class = Rogue
+elif player1_class == 'Wizard':
+    player1_class = Wizard
+else:
+    print('Вы ввели несуществующий класс персонажа')
+
