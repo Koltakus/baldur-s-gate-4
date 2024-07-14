84b30b99fbc3d37bc0957ea0aaaa5c394415d80b
Igor Koltakov <igor-koltakov@yandex.ru>
2024-07-14 11:59:56 +0300

    test2

--- bin/create_players.py
+++ bin/create_players.py
@@ -10,12 +10,16 @@ print('Приветствуем тебя,' + player2_name)
 print('Выберите класс своего персонажа,' + player1_name + '!' + '\nВыбери из предложенных ниже классов')
 print('Warrior'+'\nRouge'+'\nWizard')
 player1_class = input()
-if player1_class == 'Warrior':
-    player1_class = Warrior
-elif player1_class == 'Rogue':
-    player1_class = Rogue
-elif player1_class == 'Wizard':
-    player1_class = Wizard
-else:
-    print('Вы ввели несуществующий класс персонажа')
+def player1_class_choice():
+    if player1_class == 'Warrior':
+        player1_class = 'Warrior'
+    elif player1_class == 'Rogue':
+        player1_class = 'Rogue'
+    elif player1_class == 'Wizard':
+        player1_class = 'Wizard'
+    else:
+        print('Вы ввели несуществующий класс персонажа')
+    return player1_class
+
+print(player1_class_choice())
 
