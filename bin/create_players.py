313cd5e10990225be467464d5fbfef21545ceb4e
Igor Koltakov <igor-koltakov@yandex.ru>
2024-07-22 20:35:11 +0300

    test4

--- bin/create_players.py
+++ bin/create_players.py
@@ -10,22 +10,19 @@ print('Приветствуем тебя,' + player2_name)
 print('Выберите класс своего персонажа,' + player1_name + '!' + '\nВыбери из предложенных ниже классов')
 print('Warrior'+'\nRouge'+'\nWizard')
 player1_class = input()
-def player1_class_choice():
-    if player1_class == 'Warrior':
-        player1_class = 'Warrior'
-    elif player1_class == 'Rogue':
-        player1_class = 'Rogue'
-    elif player1_class == 'Wizard':
-        player1_class = 'Wizard'
+def player1_class_choice(player_class):
+    if player_class == 'Warrior':
+        player_class = 'Warrior'
+    elif player_class == 'Rogue':
+        player_class = 'Rogue'
+    elif player_class == 'Wizard':
+        player_class = 'Wizard'
     else:
         print('Вы ввели несуществующий класс персонажа')
-    return player1_class
+    return player_class
 
-print(player1_name + ', ты выбрал класс' + player1_class_choice()+'.' + '\nТы уверен в своем выборе?')
+player1_class = player1_class_choice(player1_class)
 
-otvet = 0
-otvet = input()
-if otvet = '':
-    print()
-else:
-    print()
+print(player1_name + ', ты выбрал класс ' + player1_class+'.' + '\nТы уверен в своем выборе?' + '\nДа/Нет?')
+
+
--- test/test.py
+++ test/test.py
@@ -1,7 +1,15 @@
 import random
 
-random_d10 = random.randint(0,12)
-i = 0
-while i < random_d10:
-    print(i)
-#    i += '❤'
+random_d12 = random.randint(1,12)
+health_bar = random_d12 * '❤'
+print(health_bar)
+
+health_warrior = []
+for x in range(1, random_d12):
+    health_warrior = []
+    for y in range(random_d12):
+        health_warrior.append(y)
+    health_warrior.append(health_warrior)
+print(health_warrior)
+
+
